bd2489ea72d87920a96a1e178b9dda28a3c522ba
karkir0003 <karkir0003@gmail.com>
2024-11-17 23:14:47 -0500

    lint files

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,11 +4,13 @@ from helpers.aws_utils import download_file_from_s3
 from constants import BONEAWAREAI_S3_BUCKET, DATASETS_FOLDER, MURA_DATASET
 from helpers.utils import unzip_file
 
+
 def download_dataset(file, output_folder):
     download_file_from_s3(BONEAWAREAI_S3_BUCKET, file, output_folder)
-    
-    if (zipfile.is_zipfile(file)):
+
+    if zipfile.is_zipfile(file):
         unzip_file(os.path.join(output_folder, file))
+
 
 def main():
     download_dataset(MURA_DATASET, DATASETS_FOLDER)
--- src/helpers/aws_utils.py
+++ src/helpers/aws_utils.py
@@ -2,14 +2,21 @@ import boto3
 import configparser
 import os
 from botocore.exceptions import NoCredentialsError, ClientError
-from constants import BONEAWAREAI_DATA_ACCESS_ROLE, BONEAWAREAI_DATA_ACCESS_SESSION, BONEAWAREAI_S3_BUCKET, DATASETS_FOLDER
+from constants import (
+    BONEAWAREAI_DATA_ACCESS_ROLE,
+    BONEAWAREAI_DATA_ACCESS_SESSION,
+    BONEAWAREAI_S3_BUCKET,
+    DATASETS_FOLDER,
+)
+
 
 def get_aws_credentials():
     config = configparser.ConfigParser()
     config.read("config.ini")
-    aws_access_key_id=config['default']["aws_access_key_id"]
-    aws_secret_access_key=config['default']["aws_secret_access_key"]
+    aws_access_key_id = config["default"]["aws_access_key_id"]
+    aws_secret_access_key = config["default"]["aws_secret_access_key"]
     return aws_access_key_id, aws_secret_access_key
+
 
 def assume_role(role_arn, session_name, duration=3600):
     """
@@ -18,19 +25,17 @@ def assume_role(role_arn, session_name, duration=3600):
     """
     aws_access_key_id, aws_secret_access_key = get_aws_credentials()
     sts_client = boto3.client(
-        'sts', 
+        "sts",
         aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
+        aws_secret_access_key=aws_secret_access_key,
     )
 
     try:
         response = sts_client.assume_role(
-            RoleArn=role_arn,
-            RoleSessionName=session_name,
-            DurationSeconds=duration 
+            RoleArn=role_arn, RoleSessionName=session_name, DurationSeconds=duration
         )
-        
-        credentials = response['Credentials']
+
+        credentials = response["Credentials"]
         return credentials
     except ClientError as e:
         print(f"Error assuming role: {e}")
@@ -39,26 +44,28 @@ def assume_role(role_arn, session_name, duration=3600):
 def download_file_from_s3(bucket_name, file_key, output_folder):
     """
     Download a file from S3 using the provided temporary credentials.
-    
+
     Params:
     - bucket name: name of S3 bucket
     - file_key: path within s3 bucket for specific file
     - output_folder: where to download the file to
     """
     # Get temporary credentials
-    credentials = assume_role(BONEAWAREAI_DATA_ACCESS_ROLE, BONEAWAREAI_DATA_ACCESS_SESSION)
-    
+    credentials = assume_role(
+        BONEAWAREAI_DATA_ACCESS_ROLE, BONEAWAREAI_DATA_ACCESS_SESSION
+    )
+
     # Use the temporary credentials to create an S3 client
     s3_client = boto3.client(
-        's3',
-        aws_access_key_id=credentials['AccessKeyId'],
-        aws_secret_access_key=credentials['SecretAccessKey'],
-        aws_session_token=credentials['SessionToken']
+        "s3",
+        aws_access_key_id=credentials["AccessKeyId"],
+        aws_secret_access_key=credentials["SecretAccessKey"],
+        aws_session_token=credentials["SessionToken"],
     )
-    
-    if (not os.path.isdir(output_folder)):
+
+    if not os.path.isdir(output_folder):
         os.makedirs(output_folder)
-    
+
     download_path = os.path.join(output_folder, file_key)
 
     try:
